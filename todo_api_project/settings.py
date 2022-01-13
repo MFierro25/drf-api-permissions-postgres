809bc531745a06784c71af919e24ecb134635a5d
Miguel Fierro <emfierro2@gmail.com>
2022-01-12 19:35:11 -0800

    created docker files

--- todo_api_project/settings.py
+++ todo_api_project/settings.py
@@ -25,7 +25,9 @@ SECRET_KEY = 'django-insecure-#b-&+9b9lpsp9k5yhdgj4(k2=tk34-)1dctfjj!hy78gl8+x5i
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '0.0.0.0',
+]
 
 
 # Application definition
